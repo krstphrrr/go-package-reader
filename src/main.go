e6700a208f9d26feb27eacdfaf1b4343e8caa4cc
Kris! <kristopher.bonefont@gmail.com>
2024-03-14 12:05:47 -0600

    test

--- src/main.go
+++ src/main.go
@@ -7,6 +7,10 @@ import (
 
 func main() {
 	path := os.Args[1]
-	// result := GetPackageVersion(path)
-	fmt.Println(GetPackageVersion(path))
+
+	// fmt.Printf("Load package.json at %s\n", path)
+
+	result := GetPackageVersion(path)
+
+	fmt.Printf("::set-output name=version::%s", result)
 }
